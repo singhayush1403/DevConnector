74e07ede0cb37068f190423feb741c222f0937bd
Ayush Singh <singhayush1403@gmail.com>
2020-10-18 11:18:09 +0530

    added mongoDB connection,User Model and Custom JWT middleware

---
+++ routes/api/auth.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth')
+router.get('/',(req,res)=>res.send('Auth Route'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-
+const connectDB = require('./config/db');
+connectDB();
 app.get('/',(req,res)=>res.send('Api running'));
 
 const PORT = process.env.PORT || 5000
+app.use(express.json())
+//Define Routes
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/post',require('./routes/api/post'))
 
 app.listen(PORT, () => console.log(PORT));
